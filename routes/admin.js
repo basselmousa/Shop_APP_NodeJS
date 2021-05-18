76ed2154aa6f225cc02e25b34366fe4f7088a556
bassel-mousa <--get>
2021-05-19 01:26:03 +0300

    Initializing Git

---
+++ routes/admin.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express');
+const router = express.Router()
+
+const rootDir = require('../util/path')
+
+const products = [];
+
+router.get('/add-product',(req, res)=>{
+    res.sendFile(path.join(rootDir,'views','add-product.html'))
+})
+
+router.post('/add-product', (req, res)=>{
+    products.push({
+        title: req.body.title
+    })
+    res.redirect('/')
+})
+
+exports.routes = router;
+exports.products = products;
