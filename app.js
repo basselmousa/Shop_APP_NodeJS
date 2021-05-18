260b25ee43524fe8d0ed56455c76ca0b6e3c52bf
bassel-mousa <--get>
2021-05-19 01:46:40 +0300

    Copy Files From Max

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const path = require('path')
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const path = require('path');
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.set('view engine', 'pug')
-app.set('views' ,'views')
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')))
-app.use("/admin",adminRoutes.routes)
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(shopRoutes)
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use((req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-app.listen(3000)
+app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,22 @@
-const path = require('path')
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
 
-const rootDir = require('../util/path')
-const adminData = require('./admin')
-
-/**  Exact match With Path */
 router.get('/', (req, res, next) => {
     const products = adminData.products;
     res.render('shop', {
-        prods:products,
-        docTitle: "Shop"
-    })
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
 });
 
-module.exports = router
+module.exports = router;
