220c5d6d58e875271eec4d8f0b711d4f119dd5fa
bassel-mousa <--get>
2021-06-20 01:56:39 +0300

    Making Methods To Handle Edit Product Requests

--- controllers/admin/ProductsController.js
+++ controllers/admin/ProductsController.js
@@ -1,12 +1,30 @@
 const Product = require('../../models/product')
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
+        editing: false
     });
+};
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit
+
+    if (!editMode){
+        return res.redirect('/')
+    }
+    const productId = req.params.id
+    Product.fetchById(productId, (product)=>{
+        if (!product){
+            return res.redirect('/')
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        });
+    } )
+
 };
 
 exports.postAddProduct = (req, res, next) => {
@@ -29,3 +47,7 @@ exports.getProducts = (req, res, next) => {
         });
     });
 };
+
+exports.postEditProduct = (req, res, next) =>{
+
+}
