cbbe8a3a019d1d6f1511ec1f533a0b252cce6841
bassel-mousa <--get>
2021-06-19 16:53:55 +0300

    Add Shop Routes

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
-const productsController = require('../controllers/admin/products');
-
+const shopController = require('../controllers/shop/ShopController');
+const cartController = require('../controllers/shop/CartController')
+const ordersController = require('../controllers/shop/OrdersController')
+const indexController = require('../controllers/shop/IndexController')
+const checkoutController = require('../controllers/shop/CheckoutController')
 const router = express.Router();
 
-router.get('/', productsController.getProducts);
+router.get('/', indexController.getIndex);
+router.get('/products', shopController.getProducts);
+router.get('/product/:productId', shopController.getProduct);
+router.get('/cart', cartController.getCarts)
+router.get('/orders', ordersController.getOrders)
+router.get('/checkout', checkoutController.getCheckout)
 
 module.exports = router;
