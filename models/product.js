23f5efcdd77c88f1def396efc606d7077f3f7803
bassel-mousa <--get>
2021-06-28 03:39:31 +0300

    Initialize Products Table By Sequelize

--- models/product.js
+++ models/product.js
@@ -1,110 +1,145 @@
-/**
- * These For Read and Write From File
- const fs = require('fs')
- const path = require('path')
- const pathHelper = require('../util/path')
- const myPath = path.join(pathHelper, 'data', 'products.json')
- */
+const Sequelize = require('sequelize')
+const sequelize = require('../util/database')
+const User = require("./user");
 
-// for connecting to database
-const db = require('../util/database')
-const Cart = require('./cart')
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
 
-/**
- * these method for get from file
- const getProductsFromFile = (callback) => {
-
-    fs.readFile(myPath, (err, fileContent) => {
-        if (err) {
-            return callback([])
-        }
-        callback(JSON.parse(fileContent))
-    })
-}
- */
-// https://www.publicdomainpictures.net/pictures/20000/t2/baby-lamb.jpg
-module.exports = class Product {
-
-    constructor(id, title, imageURL, description, price) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.imageURL = imageURL;
-
+    },
+    description:{
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    imageUrl: {
+        type:Sequelize.STRING,
+        allowNull: false,
     }
+});
 
-    save() {
-        /**
-         * These for save to file and update existing one from file
-         getProductsFromFile(products => {
-            if (this.id) {
-                const existingProductIndex = products.findIndex(prod => prod.id === this.id)
-                const updatedProduct = [...products]
-                updatedProduct[existingProductIndex] = this
-                fs.writeFile(myPath, JSON.stringify(updatedProduct), (err) => {
-                    console.log(err)
-                })
-            } else {
-                this.id = Math.random().toString();
-                products.push(this)
-                fs.writeFile(myPath, JSON.stringify(products), (err) => {
-                    console.log(err)
-                })
-            }
-        })
-         */
 
-        return db.execute("INSERT INTO products (title, price, description,imageUrl) VALUES (?,?,?,?)", [
-            this.title,
-            this.price,
-            this.description,
-            this.imageURL
-        ]);
-    }
+module.exports = Product;
 
-    static fetchAll() {
-        /**
-         * these for get all from products file
-         getProductsFromFile(callback)
-         */
 
-        // these for fetch from database
-        return db.execute('SELECT * FROM products');
-
-    }
-
-    static fetchById(id) {
-        /**
-         * these for get by id from products file
-         getProductsFromFile(products => {
-            const product = products.find(prod => prod.id === id);
-            callback(product);
-
-        })
-         */
-        return db.execute('SELECT * FROM products WHERE products.id = ?',[
-            id
-        ]);
-    }
-
-    static deleteProduct(id) {
-        /**
-         * these for delete product from file
-         getProductsFromFile(products => {
-            const product = products.find(prod => prod.id === id);
-            const updatedProducts = products.filter(prod => prod.id !== id);
-
-            fs.writeFile(myPath, JSON.stringify(updatedProducts), (err) => {
-                if (!err){
-                    Cart.deleteProduct(id, product.price);
-                }
-
-                console.log(err)
-            })
-
-        })
-         */
-    }
-
-}
+/** Old Way To Model {With out Sequelize} */
+// /**
+//  * These For Read and Write From File
+//  const fs = require('fs')
+//  const path = require('path')
+//  const pathHelper = require('../util/path')
+//  const myPath = path.join(pathHelper, 'data', 'products.json')
+//  */
+//
+// // for connecting to database
+// const db = require('../util/database')
+// const Cart = require('./cart')
+//
+// /**
+//  * these method for get from file
+//  const getProductsFromFile = (callback) => {
+//
+//     fs.readFile(myPath, (err, fileContent) => {
+//         if (err) {
+//             return callback([])
+//         }
+//         callback(JSON.parse(fileContent))
+//     })
+// }
+//  */
+// // https://www.publicdomainpictures.net/pictures/20000/t2/baby-lamb.jpg
+// module.exports = class Product {
+//
+//     constructor(id, title, imageURL, description, price) {
+//         this.id = id;
+//         this.title = title;
+//         this.description = description;
+//         this.price = price;
+//         this.imageURL = imageURL;
+//
+//     }
+//
+//     save() {
+//         /**
+//          * These for save to file and update existing one from file
+//          getProductsFromFile(products => {
+//             if (this.id) {
+//                 const existingProductIndex = products.findIndex(prod => prod.id === this.id)
+//                 const updatedProduct = [...products]
+//                 updatedProduct[existingProductIndex] = this
+//                 fs.writeFile(myPath, JSON.stringify(updatedProduct), (err) => {
+//                     console.log(err)
+//                 })
+//             } else {
+//                 this.id = Math.random().toString();
+//                 products.push(this)
+//                 fs.writeFile(myPath, JSON.stringify(products), (err) => {
+//                     console.log(err)
+//                 })
+//             }
+//         })
+//          */
+//
+//         return db.execute("INSERT INTO products (title, price, description,imageUrl) VALUES (?,?,?,?)", [
+//             this.title,
+//             this.price,
+//             this.description,
+//             this.imageURL
+//         ]);
+//     }
+//
+//     static fetchAll() {
+//         /**
+//          * these for get all from products file
+//          getProductsFromFile(callback)
+//          */
+//
+//         // these for fetch from database
+//         return db.execute('SELECT * FROM products');
+//
+//     }
+//
+//     static fetchById(id) {
+//         /**
+//          * these for get by id from products file
+//          getProductsFromFile(products => {
+//             const product = products.find(prod => prod.id === id);
+//             callback(product);
+//
+//         })
+//          */
+//         return db.execute('SELECT * FROM products WHERE products.id = ?',[
+//             id
+//         ]);
+//     }
+//
+//     static deleteProduct(id) {
+//         /**
+//          * these for delete product from file
+//          getProductsFromFile(products => {
+//             const product = products.find(prod => prod.id === id);
+//             const updatedProducts = products.filter(prod => prod.id !== id);
+//
+//             fs.writeFile(myPath, JSON.stringify(updatedProducts), (err) => {
+//                 if (!err){
+//                     Cart.deleteProduct(id, product.price);
+//                 }
+//
+//                 console.log(err)
+//             })
+//
+//         })
+//          */
+//     }
+//
+// }
