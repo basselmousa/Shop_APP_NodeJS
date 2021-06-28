9ee7078fd44f4cf7f72d99597066512f97c5b0dd
bassel-mousa <--get>
2021-06-29 02:00:09 +0300

    Making mongo Connection

---
+++ util/mongoDatabase.js
@@ -0,0 +1,30 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+
+    MongoClient.connect('mongodb+srv://bassel:mlD0mm1htbQbW74P@nodejs-shop-app.f915l.mongodb.net/shop-app?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('Connected')
+            _db = client.db();
+            callback(client);
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
+
+};
+
+const getDb = ()=>{
+    if (_db){
+        return _db;
+    }
+    throw 'No Database Found';
+}
+
+exports.mongoConnect = mongoConnect;
+
+exports.getDb = getDb;
