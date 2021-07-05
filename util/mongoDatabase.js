2996755fc6fbdff9896c6d0ceb6b6fc653b0695c
bassel-mousa <--get>
2021-07-05 04:57:15 +0300

    Adding Some Options To Avoid Deprecated Version

--- util/mongoDatabase.js
+++ util/mongoDatabase.js
@@ -5,7 +5,9 @@ let _db;
 
 const mongoConnect = (callback) => {
 
-    MongoClient.connect('mongodb+srv://bassel:mlD0mm1htbQbW74P@nodejs-shop-app.f915l.mongodb.net/shop-app?retryWrites=true&w=majority')
+    MongoClient.connect('mongodb+srv://bassel:mlD0mm1htbQbW74P@nodejs-shop-app.f915l.mongodb.net/shop-app?retryWrites=true&w=majority',{
+        useNewUrlParser: true, useUnifiedTopology: true
+    })
         .then(client => {
             console.log('Connected')
             _db = client.db();
