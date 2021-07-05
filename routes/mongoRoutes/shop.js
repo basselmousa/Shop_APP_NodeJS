df08e662dbbc99aeb8123625fb93bc9f463f5587
bassel-mousa <--get>
2021-07-06 02:25:52 +0300

    Making Orders Routes Work Again

--- routes/mongoRoutes/shop.js
+++ routes/mongoRoutes/shop.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const shopController = require('../../controllers/mongoControllers/shop/ShopController');
 const cartController = require('../../controllers/mongoControllers/shop/CartController');
-// const ordersController = require('../../controllers/mongoControllers/shop/OrdersController');
+const ordersController = require('../../controllers/mongoControllers/shop/OrdersController');
 const indexController = require('../../controllers/mongoControllers/shop/IndexController');
 // const checkoutController = require('../../controllers/mongoControllers/shop/CheckoutController');
 const router = express.Router();
@@ -19,8 +19,8 @@ router.post('/cart', cartController.postCart)
 router.post('/cart-delete-item', cartController.deleteCartItem)
 //
 //
-// router.get('/orders', ordersController.getOrdersMethod)
-// router.post('/create-order', ordersController.postOrders);
+router.get('/orders', ordersController.getOrdersMethod)
+router.post('/create-order', ordersController.postOrders);
 //
 // router.get('/checkout', checkoutController.getCheckout)
 
