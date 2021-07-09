e89c94a5b340b2ed5ca33f29aeaa229862fe7922
bassel-mousa <--get>
2021-07-10 01:35:02 +0300

    Finish Mongoose Admin Routes

--- routes/mongooseRoutes/admin.js
+++ routes/mongooseRoutes/admin.js
@@ -2,25 +2,25 @@ const path = require('path');
 
 const express = require('express');
 
-const adminProductController = require('../../controllers/mongoControllers/admin/ProductsController');
+const adminProductController = require('../../controllers/mongooseControllers/admin/ProductsController');
 
 const router = express.Router();
 
 
 
-// // /admin/add-product => GET
-// router.get('/add-product', adminProductController.getAddProduct);
-//
-// // /admin/products => GET
-// router.get('/products', adminProductController.getProducts);
+// /admin/add-product => GET
+router.get('/add-product', adminProductController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminProductController.getProducts);
 //
 // // /admin/add-product => POST
-// router.post('/add-product', adminProductController.postAddProduct);
+router.post('/add-product', adminProductController.postAddProduct);
 //
-// router.get('/edit-product/:id', adminProductController.getEditProduct);
+router.get('/edit-product/:id', adminProductController.getEditProduct);
 // //
-// router.post('/edit-product', adminProductController.postEditProduct);
+router.post('/edit-product', adminProductController.postEditProduct);
 // //
-// router.post('/delete-product', adminProductController.deleteProduct);
+router.post('/delete-product', adminProductController.deleteProduct);
 module.exports = router;
 
