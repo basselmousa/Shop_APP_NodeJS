94865fbf44d523832a55f5ee06a3dfaa48bd7baf
bassel-mousa <--get>
2021-07-11 03:53:04 +0300

    Require Auth Routes

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/mongooseRoutes/admin');
 // const shopRoutes = require('./routes/shop');
 const shopRoutes = require('./routes/mongooseRoutes/shop');
+const authRoutes = require('./routes/mongooseRoutes/auth');
 /**
  * Create Dummy User With Sequelize
 app.use((req, res, next) => {
@@ -71,6 +72,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorsController.notFound404Error);
 
