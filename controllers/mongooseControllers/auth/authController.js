b9f5ba643094c74effa9191321efeec4e6f05580
bassel-mousa <--get>
2021-07-15 05:06:48 +0300

    Making Sign Up Functionality And Make New User

--- controllers/mongooseControllers/auth/authController.js
+++ controllers/mongooseControllers/auth/authController.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require("../../../models/mongooseModels/user");
 exports.getLogin = (req, res, next) => {
 
@@ -10,10 +12,10 @@ exports.getLogin = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
 
     User.findById('60e847e38559d83b9c4f82de')
-        .then(user=>{
+        .then(user => {
             req.session.user = user;
             req.session.isLoggedIn = true;
-            req.session.save((err) =>{
+            req.session.save((err) => {
                 console.log(err)
                 res.redirect('/');
             })
@@ -31,10 +33,39 @@ exports.getSignup = (req, res, next) => {
     });
 };
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+    User.findOne({email: email})
+        .then(userDoc => {
+            if (userDoc) {
+                return res.redirect('/signup');
+            }
+            return bcrypt
+                .hash(password, 12).then(hashedPassword => {
+                    const user = new User({
+                        password: hashedPassword,
+                        email: email,
+                        cart: {
+                            items: []
+                        }
+                    });
+                    return user.save();
+                })
+                .then((_) => {
+                    res.redirect('/login')
+                });
 
-exports.postLogout = (req, res, next) =>{
-    req.session.destroy((err)=>{
+        })
+
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
         console.log(err)
         res.redirect('/');
     })
