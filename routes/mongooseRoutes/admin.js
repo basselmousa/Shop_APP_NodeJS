45254ff92d0c42059ab2a5d61441b12c5434b5cc
bassel-mousa <--get>
2021-07-19 01:09:15 +0300

    Include IsAuth Middleware

--- routes/mongooseRoutes/admin.js
+++ routes/mongooseRoutes/admin.js
@@ -4,23 +4,26 @@ const express = require('express');
 
 const adminProductController = require('../../controllers/mongooseControllers/admin/ProductsController');
 
+const isAuth = require('../../middleware/mongooseMiddleware/is-auth');
+
 const router = express.Router();
 
 
 
 // /admin/add-product => GET
-router.get('/add-product', adminProductController.getAddProduct);
+router.get('/add-product',isAuth, adminProductController.getAddProduct);
 
 // /admin/products => GET
-router.get('/products', adminProductController.getProducts);
+router.get('/products',isAuth, adminProductController.getProducts);
 //
 // // /admin/add-product => POST
-router.post('/add-product', adminProductController.postAddProduct);
+router.post('/add-product',isAuth, adminProductController.postAddProduct);
 //
-router.get('/edit-product/:id', adminProductController.getEditProduct);
+router.get('/edit-product/:id',isAuth, adminProductController.getEditProduct);
 // //
-router.post('/edit-product', adminProductController.postEditProduct);
+router.post('/edit-product',isAuth, adminProductController.postEditProduct);
 // //
-router.post('/delete-product', adminProductController.deleteProduct);
+router.post('/delete-product',isAuth, adminProductController.deleteProduct);
+
 module.exports = router;
 
--- routes/mongooseRoutes/shop.js
+++ routes/mongooseRoutes/shop.js
@@ -5,6 +5,9 @@ const cartController = require('../../controllers/mongooseControllers/shop/CartC
 const ordersController = require('../../controllers/mongooseControllers/shop/OrdersController');
 const indexController = require('../../controllers/mongooseControllers/shop/IndexController');
 // const checkoutController = require('../../controllers/mongoControllers/shop/CheckoutController');
+
+const isAuth = require('../../middleware/mongooseMiddleware/is-auth');
+
 const router = express.Router();
 
 router.get('/', indexController.getIndex);
@@ -14,13 +17,13 @@ router.get('/products', shopController.getProducts);
 router.get('/product/:productId', shopController.getProduct);
 //
 //
-router.get('/cart', cartController.getCarts)
-router.post('/cart', cartController.postCart)
-router.post('/cart-delete-item', cartController.deleteCartItem)
+router.get('/cart',isAuth, cartController.getCarts)
+router.post('/cart',isAuth, cartController.postCart)
+router.post('/cart-delete-item',isAuth, cartController.deleteCartItem)
 //
 //
-router.get('/orders', ordersController.getOrdersMethod)
-router.post('/create-order', ordersController.postOrders);
+router.get('/orders',isAuth, ordersController.getOrdersMethod)
+router.post('/create-order',isAuth, ordersController.postOrders);
 //
 // router.get('/checkout', checkoutController.getCheckout)
 
