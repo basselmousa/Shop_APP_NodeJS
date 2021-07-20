ea82f995dc4970e290791fad261c75df76712915
bassel-mousa <--get>
2021-07-21 02:03:24 +0300

    Add Routes For Resetting Password

--- routes/mongooseRoutes/auth.js
+++ routes/mongooseRoutes/auth.js
@@ -3,8 +3,21 @@ const authController = require('../../controllers/mongooseControllers/auth/authC
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
+
 router.post('/login', authController.postLogin);
+
 router.post('/logout', authController.postLogout);
+
 router.get('/signup', authController.getSignup);
+
 router.post('/signup', authController.postSignup);
+
+router.get('/reset', authController.getResetPassword);
+
+router.post('/reset', authController.postResetPassword);
+
+router.get('/reset/:token', authController.getNewPassword);
+
+router.post('/new-password', authController.postNewPassword);
+
 module.exports = router;
