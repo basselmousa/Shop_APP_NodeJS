c696e8bb1094c389300318810f243513064fede9
bassel-mousa <--get>
2021-07-27 03:32:40 +0300

    Handle Errors

--- app.js
+++ app.js
@@ -102,12 +102,14 @@ app.use((req, res, next) => {
     console.log(req.session)
     User.findById(req.session.user._id)
         .then(user => {
+            if (!user) {
+                return next();
+            }
             req.user = user;
             next();
         })
         .catch(err => {
-            // console.log(err);
-
+            throw new Error(err);
         });
 
 });
@@ -117,7 +119,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-
+app.get('/500', errorsController.somethingWentWrong500Error);
 app.use(errorsController.notFound404Error);
 
 
